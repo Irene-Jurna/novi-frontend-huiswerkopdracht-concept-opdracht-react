5bdcbb94b0e574d6bbe70462e61a47c16ee52715
Irene Jurna <irene.jurna@gmail.com>
2022-10-13 18:42:52 +0200

    opdracht 4 af

--- src/pages/subreddit/Subreddit.js
+++ src/pages/subreddit/Subreddit.js
@@ -1,19 +1,22 @@
-import React, {useEffect} from "react";
-import { Link } from 'react-router-dom';
+import React, {useEffect, useState} from "react";
+import {Link} from 'react-router-dom';
 import {useParams} from "react-router-dom";
 import axios from "axios";
+import './Subreddit.css'
 
 function Subreddit() {
-    const { subredditId } = useParams();
+    const {subredditId} = useParams();
+    const [subreddit, setSubreddit] = useState('');
 
-   // Maak hier je GET request(met${subredditId}). https://www.reddit.com/r/$subredditId}/about.json
+    // Maak hier je GET request(met${subredditId}). https://www.reddit.com/r/$subredditId}/about.json
 
     useEffect(() => {
         async function fetchSubreddit() {
 
             try {
                 const response = await axios.get(`https://www.reddit.com/r/${subredditId}/about.json`);
-                console.log(response);
+                console.log(response.data.data);
+                setSubreddit(response.data.data);
             } catch (e) {
                 console.error(e);
             }
@@ -24,9 +27,19 @@ function Subreddit() {
 
     return (
         <>
-            <p>Subreddit page</p>
-            <p>{subredditId}</p>
-            <p>Take me back <Link to="/">Home</Link> </p>
+            <section className="header-container">
+                <h1>r/{subredditId}</h1>
+                <h6>Subreddit specifications</h6>
+            </section>
+            <section className="subreddit-container">
+                <h3>Title</h3>
+                <p>{subredditId}</p>
+                <h3>Description</h3>
+                <p>{subreddit.public_description}</p>
+                <h3>Number of subscribers</h3>
+                <p className="end-of-subreddit-page">{subreddit.subscribers}</p>
+                <Link to="/">Take me back</Link>
+            </section>
         </>
     );
 }
